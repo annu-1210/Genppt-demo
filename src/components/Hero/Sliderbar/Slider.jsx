bd8b88aaf9433947d97f9a9e8e56c17124173d45
annu-1210 <annupoonia086@gmail.com>
2024-09-07 23:40:05 +0530

    content updated

--- src/components/Hero/Sliderbar/Slider.jsx
+++ src/components/Hero/Sliderbar/Slider.jsx
@@ -20,8 +20,27 @@ const images = [
 function Slider() {
   return (
     <>
-      <div className="mb-10">
+      <div className="container">
         <div className="w-[1260px] h-[1210px] bg-circleYellow z-0 top-[380px] left-[70px] absolute rounded-full"></div>
+        <div className="w-[295px] h-[50px] absolute z-20 top-[455px] left-[870px] flex justify-center items-center rounded-md shadow-tooltip1 font-Elza text-lg bg-gradient-to-r from-linearGradient1 to-linearGradient2">
+          <span>Create a presentation about honey</span>
+        </div>
+        <div className="w-[386px] h-[79px] absolute z-20 top-[520px] left-[745px] flex gap-4 bg-tooltipBg rounded-xl py-3 px-2.5 shadow-tooltipS font-Elza opacity-100 ">
+          <div className="">
+            <img src="./np-icon.png" alt="np-icon" />
+          </div>
+          <div className="flex flex-col">
+            <span className="text-xl font-semibold font-Elza">
+              New Presentation
+            </span>
+            <span className="text-base">
+              A new presentation has been generated!
+            </span>
+          </div>
+        </div>
+        <div className="absolute z-20 top-[540px] left-[780px]">
+          <img src="./curve.png" alt="arrow" />
+        </div>
         <div className="flex mt-12 max-w-[1924px] h-[350px] gap-x-2 z-10 absolute">
           {images.map((img) => {
             return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,27 @@ export default {
     extend: {
       fontFamily: {
         Elza: ['"elza"', "sans-serif"]
+      },
+      colors: {
+        orange: 'rgba(239, 108, 0, 1)',
+        yellow: 'rgba(231, 167, 5, 1)',
+        circleYellow: 'rgba(255, 215, 112, 1)',
+        textBlackColor: 'rgba(72, 68, 70, 1)',
+        textWhiteColor: 'rgba(255, 247, 239, 1)',
+        black: 'rgba(0, 0, 0, 1)',
+        white: 'rgb(255, 255, 255)',
+        tooltipBg: 'rgba(255, 247, 239, 0.8)',
+        linearGradient1: 'rgba(255, 234, 217, 0.7) ',
+        linearGradient2: 'rgba(255, 234, 217, 0.4)',
+
+      },
+      boxShadow: {
+        'tooltipS': '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+        'tooltip1': '0px 20px 40px 0px rgba(0, 0, 0, 0.1)',
+
       }
     },
-    colors: {
-      orange: 'rgba(239, 108, 0, 1)',
-      yellow: 'rgba(231, 167, 5, 1)',
-      circleYellow: 'rgba(255, 215, 112, 1)',
-      textBlackColor: 'rgba(72, 68, 70, 1)',
-      textWhiteColor: 'rgba(255, 247, 239, 1)',
-      black: 'rgba(0, 0, 0, 1)',
-      white: 'rgb(255, 255, 255)',
 
-    }
   },
   plugins: [],
 }
