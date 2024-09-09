499921df8c3d0f9fd6da2e364d198b8e9a47c90b
annu-1210 <annupoonia086@gmail.com>
2024-09-09 18:09:31 +0530

    content updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div className="relative bg-blue-500 top-[307px] bg-Footerbg h-[332px] md:h-[201px]">
+    <div className="relative top-[307px] bg-Footerbg h-[332px] md:h-[201px]">
       <div></div>
       <div></div>
       <div></div>
