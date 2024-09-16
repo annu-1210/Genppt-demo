3e04eeb9f4e2d0ed326bfd686f09ca8309512b62
annu-1210 <annupoonia086@gmail.com>
2024-09-16 16:32:54 +0530

    content updated

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -3,7 +3,7 @@ import ReviewCards from "./ReviewCards";
 
 function Reviews() {
   return (
-    <section className="max-w-[1440px] mobile:pt-24 relative mx-auto bottom-0 bg-bottom bg-no-repeat  bg-ellipse">
+    <section className="max-w-[1440px] mobile:pt-24 relative mx-auto bg-bottom bg-no-repeat  bg-ellipse overflow-hidden">
       <div className="container flex flex-col items-center justify-center gap-12 mx-auto py-14 mobile:py-20">
         <Head />
         <ReviewCards />
